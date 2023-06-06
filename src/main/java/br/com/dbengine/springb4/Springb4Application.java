f2c6d3d3ce4e8325c16182c4b75e495886c6471a
Bisterço <rodrigo.bisterco@gmail.com>
2023-06-06 09:27:31 -0300

    inicial

---
+++ src/main/java/br/com/dbengine/springb4/Springb4Application.java
@@ -0,0 +1,13 @@
+package br.com.dbengine.springb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springb4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springb4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbengine/springb4/Springb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbengine.springb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springb4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
