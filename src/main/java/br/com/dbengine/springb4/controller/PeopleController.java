4b26a9134ecb9b87a2ad6b2a5b8f6774826a9e56
Bisterço <rodrigo.bisterco@gmail.com>
2023-06-16 10:55:42 -0300

    v2.3 - people

--- src/main/java/br/com/dbengine/springb4/controller/PeopleController.java
+++ src/main/java/br/com/dbengine/springb4/controller/PeopleController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
 //import com.example.dto.PeriodoDTO;
 
 @Controller
@@ -18,24 +24,45 @@ public class PeopleController {
         return "people";
     }
 
+    @RequestMapping(value="/people/refresh")
+    public String peopleRefresh(RedirectAttributes redirectAttributes)
+    {
+        redirectAttributes.addAttribute("refresh", "true");
+        //redirectAttributes.addFlashAttribute("fa", faValue);
+        return "redirect:/people";
+    }
+
     @GetMapping("/people/add")
     public String peopleAdd() {
         return "peopleAdd";
     }
 
     @GetMapping("/people/update")
-    public String peopleUpdate(@RequestParam(name="Nome") String name,
+    public String peopleUpdate(@RequestParam(name="id") String id,
                                @RequestParam(name="codigoInterno") String codigoInterno,
-                               @RequestParam(name="id") String id,
+                               @RequestParam(name="nome") String name,
+                               @RequestParam(name="dataNascimento") String dataNascimento,
+                               @RequestParam(name="observacao") String observacao,
                                Model model) {
         //model.addAttribute("name", name);
         //System.out.println("id " + id );
-        //System.out.println("Nome " + name );
+        //System.out.println("Nome ANTES decode: " + name );
         //System.out.println("codigoInterno " + codigoInterno );
+        name = this.decode(name);
+        //System.out.println("Nome DEPOIS decode: " + name );
         model.addAttribute("peopleId", id);
         model.addAttribute("name", name);
+        model.addAttribute("dataNascimento", dataNascimento);
+        model.addAttribute("observacao", observacao);
         model.addAttribute("codigoInterno", codigoInterno);
         return "peopleUpdate";
     }
 
+    private String decode(String value) {
+        try {
+            return URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
