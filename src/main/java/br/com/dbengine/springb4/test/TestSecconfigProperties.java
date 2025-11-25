57f160613964e3ff97f7d7f9bb25fc5e48fc4650
Bisterço <rodrigo.bisterco@gmail.com>
2025-11-25 10:25:12 -0300

    depois dos ajustes

--- src/main/java/br/com/dbengine/springb4/test/TestSecconfigProperties.java
+++ src/main/java/br/com/dbengine/springb4/test/TestSecconfigProperties.java
@@ -1,26 +1,26 @@
 package br.com.dbengine.springb4.test;
 
-import br.com.dbengine.springb4.dbUtil.*;
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.test.context.*;
-import org.springframework.test.context.junit4.*;
+//import br.com.dbengine.springb4.dbUtil.*;
+//import org.junit.*;
+//import org.junit.runner.*;
+//import org.springframework.beans.factory.annotation.*;
+//import org.springframework.test.context.*;
+//import org.springframework.test.context.junit4.*;
 
-import static org.junit.Assert.assertThat;
+//import static org.junit.Assert.assertThat;
 
 
-@RunWith(SpringRunner.class)
-//@TestPropertySource("/secconfig.properties")
-@TestPropertySource("classpath:secconfig.properties")
+//@RunWith(SpringRunner.class)
+////@TestPropertySource("/secconfig.properties")
+//@TestPropertySource("classpath:secconfig.properties")
 public class TestSecconfigProperties {
 
-    @Value("${canonicKey}")
-    private String foo;
-
-    @Test
-    public void whenFilePropertyProvided_thenProperlyInjected() {
-        System.out.println(">>" + foo);
-        //assertThat(foo).isEqualTo("bar");
-    }
+//    @Value("${canonicKey}")
+//    private String foo;
+//
+//    @Test
+//    public void whenFilePropertyProvided_thenProperlyInjected() {
+//        System.out.println(">>" + foo);
+//        //assertThat(foo).isEqualTo("bar");
+//    }
 }
